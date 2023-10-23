4ec66cfea937a43735abe1da0c8aab5800db2fd5
Henrique Eduardo <henrique_eduardo_souza@hotmail.com>
2023-10-23 14:09:54 -0300

    💥 NEW: react router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import InitialPage from './pages/initialPage';
 import Competitions from './pages/competitions';
 import Practice from './pages/practice';
@@ -8,7 +10,15 @@ import AboutUs from './pages/aboutUs';
 
 function App() {
   return (
-    <InitialPage></InitialPage>
+    <Router>
+      <Routes>
+        <Route path="/" element={<InitialPage/>}/>
+        <Route path="/competicoes" element={<Competitions/>}/>
+        <Route path="/pratique" element={<Practice/>}/>
+        <Route path="/historico" element={<Historic/>}/>
+        <Route path="/sobrenos" element={<AboutUs/>}/>
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
 class Navbar extends React.Component{
     render(){
         return (
             <nav>
-                <h1>Polibits</h1>
+                <Link to='/'><h1>Polibits</h1></Link>
                 <ul>
-                    <li><a href='/competicoes'>competições</a></li>
-                    <li><a href='/pratique'>pratique</a></li>
-                    <li><a href='/historico'>histórico</a></li>
-                    <li><a href='/sobrenos'>sobre nos</a></li>
+                    <li><Link to='/competicoes'>COMPETIÇÕES</Link></li>
+                    <li><Link to='/pratique'>PRATIQUE</Link></li>
+                    <li><Link to='/historico'>HISTÓRICO</Link></li>
+                    <li><Link to='/sobrenos'>SOBRE NÓS</Link></li>
                 </ul>
             </nav>
         );
