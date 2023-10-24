2272e504d5884548491a3523981d6ffcd24ae170
Henrique Eduardo <henrique_eduardo_souza@hotmail.com>
2023-10-24 09:21:53 -0300

    📱 Design: i don't know what im doing

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -14,15 +14,6 @@ class Footer extends React.Component {
                         <div className='logo-img'>
                             <img src={polibitsLogo}></img>
                         </div>
-                        <div className='social-media' id='footer-social-media'>
-                            <h3>redes sociais</h3>
-                            <ul>
-                                <li><Link to='https://www.instagram.com/polibits.usp/'>instagram</Link></li>
-                                <li><Link to='https://github.com/Polibits'>github</Link></li>
-                                <li><Link to='https://discord.gg/arezHqz3'>discord</Link></li>
-                                <li><Link to='/'>whatsapp</Link></li>
-                            </ul>
-                        </div>
                         <div className='important-links' id='footer-important-links'>
                             <h3>links úteis</h3>
                             <ul>
@@ -32,9 +23,20 @@ class Footer extends React.Component {
                                 <li><Link to='/sobrenos'>sobre nós</Link></li>
                             </ul>
                         </div>
+                        <div className='social-media' id='footer-social-media'>
+                            <h3>redes sociais</h3>
+                            <ul>
+                                <li><Link to='https://www.instagram.com/polibits.usp/'>instagram</Link></li>
+                                <li><Link to='https://github.com/Polibits'>github</Link></li>
+                                <li><Link to='https://discord.gg/arezHqz3'>discord</Link></li>
+                                <li><Link to='/'>whatsapp</Link></li>
+                            </ul>
+                        </div>
                         <div className="footer-contact" id='footer-contact'>
                             <h3>contato</h3>
-                            <p>polibits.usp@gmail.com</p>
+                            <ul>
+                                <li><p>polibits.usp@gmail.com</p></li>
+                            </ul>
                         </div>
                     </div>
                     <div className='copyright'>
