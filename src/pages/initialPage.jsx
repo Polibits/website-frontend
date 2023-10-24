701c1d9b6ccd78ed6e0c9edcb0a643c33cad3948
Henrique Eduardo <henrique_eduardo_souza@hotmail.com>
2023-10-24 09:46:39 -0300

    🚧 Work in Progress:  initial page basic blueprint

--- src/pages/initialPage.jsx
+++ src/pages/initialPage.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
 
+import '../css/initialPage.css';
+
 import Navbar from '../components/navbar';
 import Footer from '../components/footer';
+
+import polibitsLogo from '../assets/logo/LOGOTIPO POLIBITS - FUNDO TRANSPARENTE.png'
 
 class InitialPage extends React.Component{
     render(){
         return (
             <div>
                 <Navbar></Navbar>
-                <h1>Página Inicial</h1>
+                <div className='page-content'>
+                    <div className='logo-div'>
+                        <img alt='logo' src={polibitsLogo}></img>
+                    </div>
+                    <div className='post'>
+                        <h1>ACM-ICPC 2022</h1> <hr></hr>
+                        <p>texto</p>
+                    </div>
+                </div>
                 <Footer></Footer>
             </div>
         );
