dcba2e79374ea497b84814c188f27529dd3be711
Henrique Eduardo <henrique_eduardo_souza@hotmail.com>
2023-10-24 22:03:15 -0300

    🚧 Work in Progress: i think im doing something

--- src/pages/initialPage.jsx
+++ src/pages/initialPage.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import '../css/pageContent.css';
 import '../css/initialPage.css';
 
 import Navbar from '../components/navbar';
 import Footer from '../components/footer';
+
+import Post from '../components/post';
 
 import polibitsLogo from '../assets/logo/LOGOTIPO POLIBITS - FUNDO TRANSPARENTE.png'
 
@@ -11,19 +14,33 @@ class InitialPage extends React.Component{
     render(){
         return (
             <div>
-                <Navbar></Navbar>
-                <div className='page-content'>
-                    <div className='logo-div'>
-                        <img alt='logo' src={polibitsLogo}></img>
-                    </div>
-                    <div className='post'>
-                        <h1>ACM-ICPC 2022</h1> <hr></hr>
-                        <p>texto</p>
+                <div>
+                    <Navbar></Navbar>
+                    <div className='page-content'>
+                        <div className='logo-div'>
+                            <img alt='logo' src={polibitsLogo}></img>
+                        </div>
+                        <Posts></Posts>
                     </div>
                 </div>
                 <Footer></Footer>
             </div>
         );
+    }
+}
+
+class Posts extends React.Component{
+    render(){
+        return(
+            <div className='posts'>
+                <Post title='ACM-ICPC 2023' textBody='texto aleatório para teste texto aleatório para teste texto aleatório para teste texto aleatório para teste'></Post>
+                <Post title='ACM-ICPC 2023' textBody='texto aleatório para teste texto aleatório para teste texto aleatório para teste texto aleatório para teste'></Post>
+                <Post title='ACM-ICPC 2023' textBody='texto aleatório para teste texto aleatório para teste texto aleatório para teste texto aleatório para teste'></Post>
+                <Post title='ACM-ICPC 2023' textBody='texto aleatório para teste texto aleatório para teste texto aleatório para teste texto aleatório para teste'></Post>
+                <Post title='ACM-ICPC 2023' textBody='texto aleatório para teste texto aleatório para teste texto aleatório para teste texto aleatório para teste'></Post>
+                <Post title='ACM-ICPC 2023' textBody='texto aleatório para teste texto aleatório para teste texto aleatório para teste texto aleatório para teste'></Post>
+            </div>
+        )
     }
 }
 
